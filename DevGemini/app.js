7d77a495ca66637649e3faa75de2c7cd5650ec80
KAUE <139394184+kauefs@users.noreply.github.com>
2024-09-06 20:30:06 -0300

    app.js

--- DevGemini/app.js
+++ DevGemini/app.js
@@ -1,1 +1,36 @@
-
+function search() {
+    // Get HTML section where results will be shown:
+    let section     = document.getElementById('results');
+    let searchField = document.getElementById('search-field').value
+    // if searchField is empty:
+    if (!searchField) {section.innerHTML = '<p>Nothing found, try another paiting.</p>'
+                       return}
+    searchField = searchField.toLowerCase()
+    // Start empty fields to store results:
+    let results   = '';
+    let titulo    = ''; 
+    let descricao = '';
+    let tags      = '';
+    // Iterate over every item on the data list:
+    for (let item of data) {
+        titulo    = item.titulo.toLowerCase()
+        descricao = item.descricao.toLowerCase()
+        tags      = item.tags.toLowerCase()
+        // including every element in searchField:
+        if (titulo.includes(searchField) || descricao.includes(searchField) || tags.includes(searchField)) {
+            // creating new element:
+            results += `
+            <div class='item-results'>
+                <h2>
+                    <a href='#' target='_blank'>${item.titulo}</a>
+                </h2>
+                <p class='descricao-meta'>${item.descricao}</p>
+                <a href=${item.link} target='_blank'>More information</a>
+            </div>
+        `;
+        }
+    }
+    if (!results) {results = '<p>Nothing found!</p>'}
+    // Display results to HTML section:
+    section.innerHTML = results;
+};
