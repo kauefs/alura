031e57146ff8ad8d1357fe9d8c586c7becc3b9f3
KAUE <139394184+kauefs@users.noreply.github.com>
2024-11-20 19:33:03 -0300

    dbconfig.js

--- DevBackEnd/dbconfig.js
+++ DevBackEnd/dbconfig.js
@@ -1,9 +1,9 @@
-import {MongoClient} from 'mongodb';
-export default async function dbConnect(connect) {let mongoClient;
-    try {mongoClient=  new  MongoClient(connect);
-         console.log('DataBase Cluster Connection…');
-         await mongoClient.connect();
-         console.log('MongoDB  Atlas   Connected!' );
-         return mongoClient;}
-    catch (err) {console.error('Connection Error!', err);
-                 process.exit();}};
+import                     {MongoClient}        from 'mongodb'   ;
+export default async function dbConnect(connect){let  mongoClient;
+     try {mongoClient=  new MongoClient(connect) ;
+          console.log('DataBase Cluster Connection…');
+          await             mongoClient.connect();
+          console.log('MongoDB   Atlas  Connected!' );
+          return            mongoClient;}
+     catch (error) {console.error(     'Connection Error!', error);
+                    process.exit( )}};
