3de603c140239ded405edb43b3839bcc7e304336
KAUE <139394184+kauefs@users.noreply.github.com>
2024-11-22 23:52:59 -0300

    dbconfig.js

--- DevBackEnd/dbconfig.js
+++ DevBackEnd/dbconfig.js
@@ -3,7 +3,7 @@ export default async function dbConnect(connect){let  mongoClient;
      try {mongoClient=  new MongoClient(connect) ;
           console.log('DataBase Cluster Connection…');
           await             mongoClient.connect();
-          console.log('MongoDB   Atlas  Connected!' );
+          console.log('MongoDB   Atlas  Connected !');
           return            mongoClient;}
      catch (error) {console.error(     'Connection Error!', error);
                     process.exit( )}};
