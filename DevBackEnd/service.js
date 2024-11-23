30d8f39d48b5f354b8434b3d2990f72ba1f876a9
KAUE <139394184+kauefs@users.noreply.github.com>
2024-11-23 02:37:58 -0300

    service.js

--- DevBackEnd/service.js
+++ DevBackEnd/service.js
@@ -1,7 +1,7 @@
 import { GoogleGenerativeAI } from '@google/generative-ai';
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 const model =  genAI.getGenerativeModel({ model:'gemini-1.5-flash'});
-export default async function GeminiDescription(imageBuffer){const prompt='description to the following image';
+export default async function GeminiDescription(imageBuffer){const prompt='description to the following image in no more than five words';
     try {const image={inlineData:{data:imageBuffer.toString('base64'), mimeType:'image/png'}};
          const   res=await model.generateContent([prompt, image]);
     return       res.response.text() ||             'Alt-Text Description Not Available.'}
