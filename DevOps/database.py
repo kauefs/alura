d065d79f4a6cc23515f398cb31c0aed7de630f4b
KAUE <139394184+kauefs@users.noreply.github.com>
2025-07-02 22:58:15 -0300

    database.py

---
+++ DevOps/database.py
@@ -0,0 +1,14 @@
+import os
+from sqlalchemy                 import create_engine
+from sqlalchemy.orm             import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+# Caminho para o arquivo SQLite (pode ser ajustado conforme necessário):
+BASE_DIR=  os.path.dirname(os.path.abspath(__file__))
+DATABASE_URL =f'sqlite:///{os.path.join(BASE_DIR,"escola.db")}'
+engine=create_engine(DATABASE_URL, connect_args={'check_same_thread':False})
+SessionLocal =sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base=declarative_base( )
+def get_db( ):
+    db =SessionLocal ( )
+    try    :yield db
+    finally:      db.close( )
