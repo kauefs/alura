ae01c600f92c9ef89121641346cbc8237c7dca52
KAUE <139394184+kauefs@users.noreply.github.com>
2026-04-01 21:06:17 -0300

    utils.js

--- FontEndIA/utils.js
+++ FontEndIA/utils.js
@@ -3,5 +3,5 @@ export function getYouTubeId(url){
     if (url.includes('v=')){return url.split('v=')[1].split('&')[0];}
     return url.split('/').pop( );}
 export function getRandomMatchScore( ){return Math.floor(Math.random( )*20+80);}
-export function getRandomDuration(hasProgress){return hasProgress ? '10 temporadas':'2h '+Math.floor(Math.random( )*59)+'m';}
+export function getRandomDuration(hasProgress){return hasProgress ? '10 seasons':'2h '+Math.floor(Math.random( )*59)+'m';}
 export function getRandomAgeBadge  ( ){return Math.random( ) > .5 ? {text:'A16', class: 'red-accent'}:{text:'16', class:''};}
